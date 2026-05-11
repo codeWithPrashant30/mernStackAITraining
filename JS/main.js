d5aad79fa6859ec08126a81cdad7b144f9c0485b
codeWithPrashant30 <palfronted300593@gmail.com>
2026-05-11 22:41:40 +0530

    Add Day 5 - JavaScript Day 1 files with variables (var, let, const) examples

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+console.log("JS DAY 1");
+
+//variables
+
+// let const var 
+
+// ES6
+
+
+//var x
+
+// x =10
+
+
+
+var x = 10;
+
+
+{
+   var x  = 15
+}
+
+console.log("x value", x)
+
+
+console.log("using var x", x)
+
+let  y = 10
+
+{
+  let  y = 16
+}
+console.log("using let y", y)
+
+const z = 10
+
+console.log("using const z", z)
